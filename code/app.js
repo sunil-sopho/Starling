130f7fad2d28f32d0d4923f91419f9d244dbcf07
Sunil Kumar <sunilssaharan@gmail.com>
2018-04-30 16:22:19 +0530

    got stats window

--- code/app.js
+++ code/app.js
@@ -4,7 +4,7 @@ const url = require('url');
 
 // function for creating a window
 function createWindow(){
-	win = new BrowserWindow({width:1900,height:1024})
+	win = new BrowserWindow({width:800,height:624})
 
 		win.loadURL(url.format({
 			pathname: path.join(__dirname,'index.html'),
